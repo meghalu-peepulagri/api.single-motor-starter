4c92d159c1216f3baad99c2539a044c5a97dad6d
meghalu-peepulagri <saikrishna.s@orotron.com>
2025-12-27 15:30:38 +0530

    KR -- commented : config

--- src/database/configuration.ts
+++ src/database/configuration.ts
@@ -1,35 +1,34 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import pg from "pg";
 
+import dbConfig from "../config/db-config.js";
+import * as DeviceRunTimeSchema from "./schemas/device-runtime.js";
 import * as deviceTokensSchema from "./schemas/device-tokens.js";
 import * as fieldsSchema from "./schemas/fields.js";
 import * as locationsSchema from "./schemas/locations.js";
+import * as MotorRunTimeSchema from "./schemas/motor-runtime.js";
+import * as motorSchedulesSchema from "./schemas/motor-schedules.js";
 import * as motorsSchema from "./schemas/motors.js";
 import * as otpSchema from "./schemas/otp.js";
-import * as userActivityLogsSchema from "./schemas/user-activity-logs.js";
-import * as usersSchema from "./schemas/users.js";
 import * as starterBoxSchema from "./schemas/starter-boxes.js";
 import * as starterBoxParameters from "./schemas/starter-parameters.js";
-import * as motorSchedulesSchema from "./schemas/motor-schedules.js";
-import * as DeviceRunTimeSchema from "./schemas/device-runtime.js";
-import * as MotorRunTimeSchema from "./schemas/motor-runtime.js";
-import env from "../env.js";
-import fs from "fs";
+import * as userActivityLogsSchema from "./schemas/user-activity-logs.js";
+import * as usersSchema from "./schemas/users.js";
 
 
 const { Pool } = pg;
 
 const dbClient = new Pool({
-  host: env.DB_HOST,
-  port: Number(env.DB_PORT),
-  user: env.DB_USER,
-  password: env.DB_PASSWORD,
-  database: env.DB_NAME,
-  ssl: {
-    rejectUnauthorized: true,
-    ca: fs.readFileSync(`${process.cwd()}/ca.pem`).toString(),
-  },
-  // connectionString: dbConfig.connectionString,
+  // host: env.DB_HOST,
+  // port: Number(env.DB_PORT),
+  // user: env.DB_USER,
+  // password: env.DB_PASSWORD,
+  // database: env.DB_NAME,
+  // ssl: {
+  //   rejectUnauthorized: true,
+  //   ca: fs.readFileSync(`${process.cwd()}/ca.pem`).toString(),
+  // },
+  connectionString: dbConfig.connectionString,
 });
 
 const db = drizzle(dbClient, {
