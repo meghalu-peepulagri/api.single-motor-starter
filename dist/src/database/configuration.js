045a4e2a25eb4babfe2ed89cf894b997757acec3
meghalu-peepulagri <saikrishna.s@orotron.com>
2025-12-30 16:08:37 +0530

    KR -- device add : validations updated to mandatory

--- dist/src/database/configuration.js
+++ dist/src/database/configuration.js
@@ -13,18 +13,20 @@ import * as starterBoxSchema from "./schemas/starter-boxes.js";
 import * as starterBoxParameters from "./schemas/starter-parameters.js";
 import * as userActivityLogsSchema from "./schemas/user-activity-logs.js";
 import * as usersSchema from "./schemas/users.js";
+import env from "../env.js";
+import fs from "fs";
 const { Pool } = pg;
 const dbClient = new Pool({
-    // host: env.DB_HOST,
-    // port: Number(env.DB_PORT),
-    // user: env.DB_USER,
-    // password: env.DB_PASSWORD,
-    // database: env.DB_NAME,
-    // ssl: {
-    //   rejectUnauthorized: true,
-    //   ca: fs.readFileSync(`${process.cwd()}/ca.pem`).toString(),
-    // },
-    connectionString: dbConfig.connectionString,
+    host: env.DB_HOST,
+    port: Number(env.DB_PORT),
+    user: env.DB_USER,
+    password: env.DB_PASSWORD,
+    database: env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: true,
+        ca: fs.readFileSync(`${process.cwd()}/ca.pem`).toString(),
+    },
+    // connectionString: dbConfig.connectionString,
 });
 const db = drizzle(dbClient, {
     schema: {
