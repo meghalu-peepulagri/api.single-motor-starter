07ef0248b3113a58be69791f7871ae678a5f272c
meghalu-peepulagri <saikrishna.s@orotron.com>
2026-01-13 16:24:03 +0530

    KR -- payload validations analytics : writen

---
+++ src/handlers/analytics-handlers.ts
@@ -0,0 +1,25 @@
+import type { Context } from "hono";
+import { getCompletePayloadAnalytics } from "../services/db/analytics-services.js";
+import { logger } from "../utils/logger.js";
+
+export class AnalyticsHandlers {
+  async getPayloadAnalyticsHandler(c: Context) {
+    try {
+      const fromDate = c.req.query("from_date") || undefined;
+      const toDate = c.req.query("to_date") || undefined;
+      const starterId = c.req.query("starter_id") ? Number(c.req.query("starter_id")) : undefined;
+
+      const analytics = await getCompletePayloadAnalytics({ fromDate, toDate, starterId });
+
+      return c.json({
+        success: true,
+        message: "Analytics fetched successfully",
+        data: analytics
+      });
+    } catch (error: any) {
+      logger.error("Error in get Payload Analytics Handler : ", error);
+      console.error("Error at get Payload Analytics Handler : ", error)
+      throw error;
+    }
+  }
+}
