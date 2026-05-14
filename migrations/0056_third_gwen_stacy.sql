f40217840407f935d462fd7504c3caf021213f20
meghalu-peepulagri <saikrishna.s@orotron.com>
2026-05-14 18:25:12 +0530

    KR -- updated

--- migrations/0056_third_gwen_stacy.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "benched_starter_parameters" ALTER COLUMN "motor_id" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "gateways" ALTER COLUMN "name" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "starter_parameters" ALTER COLUMN "motor_id" DROP NOT NULL;
