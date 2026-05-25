7d3b5484b31467419ef44573c70854b9eee106ae
meghalu-peepulagri <saikrishna.s@orotron.com>
2026-05-25 15:27:28 +0530

    KR -- updated delete user : synaxt of user id

--- src/handlers/user-handlers.ts
+++ src/handlers/user-handlers.ts
@@ -169,7 +169,7 @@ export class UserHandlers {
   deleteUserHandler = async (c: Context) => {
     try {
       const userPayload = c.get("user_payload");
-      const userId = +c.req.param("id");
+      const userId = +c.req.param("id") || 0;
       paramsValidateException.validateId(userId, "user id");
 
       const targetUser = await getSingleRecordByMultipleColumnValues<UsersTable>(
