8e3c06dc55ef3aa591272708616ff9f837db200d
Muninaik-mude <munikumar.m@orotron.com>
2026-07-08 19:35:32 +0530

    MKN -- Update: Error / success messages

--- dist/src/database/configuration.js
+++ dist/src/database/configuration.js
@@ -1,6 +1,7 @@
 import { drizzle } from "drizzle-orm/node-postgres";
+import fs from "fs";
 import { Pool } from "pg";
-import dbConfig from "../config/db-config.js";
+import env from "../env.js";
 import * as alertsFaultsSchema from "./schemas/alerts-faults.js";
 import * as benchedStarterParametersSchema from "./schemas/benched-starter-parameters.js";
 import * as DeviceRunTimeSchema from "./schemas/device-runtime.js";
@@ -29,16 +30,16 @@ import * as subUserPermissionsSchema from "./schemas/sub-user-permissions.js";
 import * as userActivityLogsSchema from "./schemas/user-activity-logs.js";
 import * as usersSchema from "./schemas/users.js";
 const dbClient = new Pool({
-    // host: env.DB_HOST,
-    // port: Number(env.DB_PORT),
-    // user: env.DB_USER,
-    // password: env.DB_PASSWORD,
-    // database: env.DB_NAME,
-    // ssl: {
-    //   rejectUnauthorized: true,
-    //   ca: fs.readFileSync(`${process.cwd()}/ca.pem`).toString(),
-    // },
-    connectionString: dbConfig.connectionString,
+    host: env.DB_HOST,
+    port: Number(env.DB_PORT),
+    user: env.DB_USER,
+    password: env.DB_PASSWORD,
+    database: env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: true,
+        ca: fs.readFileSync(`${process.cwd()}/ca.pem`).toString(),
+    },
+    // connectionString: dbConfig.connectionString,
 });
 const db = drizzle(dbClient, {
     schema: {
